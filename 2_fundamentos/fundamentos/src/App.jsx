eefb1dc4852fd53cec6d5fcda68e570978e3de31
Jú Garcia <spacejugarcia@gmail.com>
2024-01-08 17:22:34 -0300

    update

---
+++ 2_fundamentos/fundamentos/src/App.jsx
@@ -0,0 +1,17 @@
+//components 
+import FirstCompent from './components/FirstComponent';
+
+
+//styles/ CSS
+import './App.css';
+
+function App() {
+ return (
+  <div className="App">
+   <h1>fundamentos </h1>
+   <FirstCompent />
+   </div>
+  );
+}
+
+export default App
---
+++ 2_fundamentos/fundamentos/src/components/FirstComponent.jsx
@@ -0,0 +1,9 @@
+const FirstComponent = () => {
+    return(
+        <div>
+            <h1>Meu primeiro Componente</h1>
+            <p>Lorem ipsum dolor sit amet. Vel quia omnis id sequi molestiae ab velit voluptatibus ab minima debitis hic quas nemo sed nemo voluptas. Est magnam exercitationem qui quia repellendus hic iure beatae 33 Quis amet et nihil quae.</p>
+        </div>
+    );
+}
+export default FirstComponent;
